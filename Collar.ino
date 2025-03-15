7af3809bfb3d57a265b79968c5ae7a33895efefa
Ekaterina <108530800+piccolo-gatto@users.noreply.github.com>
2025-03-15 21:21:34 +0800

    Update Collar.ino

--- Collar.ino
+++ Collar.ino
@@ -12,8 +12,8 @@
 
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
-#define SSID = "****";
-#define PASS = "********"; 
+#define SSID "****"
+#define PASS "********"
 
 
 Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified();
